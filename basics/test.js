92393ef84236770e57cbeb60625aa4c04000b188
chirag sharma <111918100+24chirag@users.noreply.github.com>
2024-06-29 09:44:09 +0000

    .

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,6 @@
 console.log("chirag sharma")
+// variables which are changable 
+// constants are not changable 
+console.log("registration")
+const accoutId=123233
+console.log("accountId is " + accountId)
