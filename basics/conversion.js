5655a0f2809b9a29023a72f4a7a691f818ead3d4
chirag sharma <111918100+24chirag@users.noreply.github.com>
2024-06-30 18:18:41 +0000

    conversion in javascript

---
+++ basics/conversion.js
@@ -0,0 +1,19 @@
+let score = null
+let valueInNumber = Number(score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)//NaN-> NOT A NUMBER //NOT STRICT CHECK
+// in case of null its will match to 0 in number 
+// "33"-> 33
+// "33abc"->NaN
+//   True->1
+//   false ->0
+//""->false
+//"chirag"->true 
+
+//*****************************************operation*********************** */
+let num = 1
+let neg = -num
+console.log(neg)
+// + is also used for concatenation 
+console.log("hello"+"chirag")
